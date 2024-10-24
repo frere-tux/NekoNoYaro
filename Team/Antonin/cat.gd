a797e2cfcc355b515543ef59a5cbe179713e857b
Antonin <frere_tux@hotmail.fr>
2024-10-24 15:26:02 +0200

    Added simple slide to the cat

--- Team/Antonin/cat.gd
+++ Team/Antonin/cat.gd
@@ -2,19 +2,34 @@ class_name Cat
 
 extends CharacterBody3D
 
+enum CatMotionState {IDLE, RUNNING, JUMPING, SLIDING}
+
+@onready var animated_sprite = $AnimatedSprite3D
+@onready var base_collision = $BaseCollision
+@onready var slide_collision = $SlideCollision
+@onready var ceiling_trigger = $CeilingTrigger
+
+@onready var debug_label = $DebugLabel
+
 @export var cat_path: CatPath
+@export var lost_path_distance: float = 1.5
 
 @export var speed: 	float = 5.0
 
-@export var lost_path_distance: float = 1.5
+var motion_state: CatMotionState = CatMotionState.IDLE
 
+# Jump
 @export var max_jump_time: 				float = 0.6
 @export var initial_jump_velocity: 		float = 3.5
 @export var continuous_jum_velocity: 	float = 5.0
-var is_jumping: 	bool = false;
-var jump_time:       float = 0.0
+var jump_time: float = 0.0
 
-@onready var animated_sprite = $AnimatedSprite3D
+# Slide
+@export var slide_speed:	float = 5.0
+@export var min_slide_time:	float = 0.5
+@export var max_slide_time:	float = 1.0
+var slide_time: float = 0.0
+
 
 var started = false
 
@@ -25,6 +40,7 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("Confirm"):
 		started = true
+		motion_state = CatMotionState.RUNNING
 
 
 func _physics_process(delta):
@@ -36,24 +52,37 @@ func _physics_process(delta):
 	if started == true:
 
 		# Handle jump.
-		if Input.is_action_just_pressed("Jump") and is_on_floor() and not is_jumping:
-			is_jumping = true
+		if motion_state != CatMotionState.JUMPING and Input.is_action_just_pressed("Jump") and is_on_floor():
+			motion_state = CatMotionState.JUMPING
 			velocity.y = initial_jump_velocity
-		elif Input.is_action_pressed("Jump") and is_jumping and jump_time < max_jump_time:
+		elif motion_state == CatMotionState.JUMPING and jump_time < max_jump_time and Input.is_action_pressed("Jump"):
 			velocity.y += continuous_jum_velocity * delta
 			jump_time+= delta
+		elif motion_state == CatMotionState.RUNNING and Input.is_action_just_pressed("Slide") and is_on_floor():
+			motion_state = CatMotionState.SLIDING
+			enable_slide_collision(true)
+		elif motion_state == CatMotionState.SLIDING and (slide_time < max_slide_time and (Input.is_action_pressed("Slide") or slide_time < min_slide_time) or ceiling_trigger.has_overlapping_bodies()):
+			slide_time += delta
 		else:
-			is_jumping = false
+			motion_state = CatMotionState.RUNNING
+			enable_slide_collision(false)
 			jump_time = 0.0
-
+			slide_time = 0.0
 		
 		# Follow path
 		var direction = cat_path.path_follow.position - position
 		direction.y = 0.0
 		var distanceToPath = direction.length()
 		
+		var current_speed
+		match motion_state:
+			CatMotionState.SLIDING:
+				current_speed = slide_speed
+			_:
+				current_speed = speed 
+		
 		if distanceToPath < lost_path_distance:
-			cat_path.path_follow.progress += speed * delta
+			cat_path.path_follow.progress += current_speed * delta
 			direction = cat_path.path_follow.position - position
 			direction.y = 0.0
 			distanceToPath = direction.length();
@@ -64,11 +93,11 @@ func _physics_process(delta):
 		direction = direction.normalized()
 		
 		if direction:
-			velocity.x = direction.x * maxf(speed, distanceToPath)
-			velocity.z = direction.z * maxf(speed, distanceToPath)
+			velocity.x = direction.x * maxf(current_speed, distanceToPath)
+			velocity.z = direction.z * maxf(current_speed, distanceToPath)
 		else:
-			velocity.x = move_toward(velocity.x, 0, speed)
-			velocity.z = move_toward(velocity.z, 0, speed)
+			velocity.x = move_toward(velocity.x, 0, current_speed)
+			velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
 	
@@ -78,14 +107,24 @@ func _physics_process(delta):
 
 func _process(delta):
 	
+	#debug_label.text = CatMotionState.keys()[motion_state]
+	#debug_label.text = "Ceiling" if ceiling_trigger.has_overlapping_bodies() else "No ceiling"
+	
 	if is_on_wall():
 		animated_sprite.play("Hit")
 	elif not is_on_floor():
-		if is_jumping and jump_time == 0.0:
+		if motion_state == CatMotionState.JUMPING and jump_time == 0.0:
 			animated_sprite.play("Jump")
 		elif velocity.y < 0.0:
 			animated_sprite.play("Fall")
+	elif motion_state == CatMotionState.SLIDING:
+		animated_sprite.play("Slide")
 	elif velocity.x != 0.0 or velocity.z != 0.0:
 		animated_sprite.play("Run")
 	else:
 		animated_sprite.play("Stand_Idle")
+		motion_state = CatMotionState.IDLE
+		
+func enable_slide_collision(enabled: bool):
+	base_collision.disabled = enabled
+	slide_collision.disabled = not enabled
